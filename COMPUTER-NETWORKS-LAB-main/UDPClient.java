458ba4c47bad587071060943957bb7e5d5170892
Haneesh Bandaru <97587849+Haneesh05@users.noreply.github.com>
2022-07-15 07:35:09 +0530

    Add files via upload

---
+++ COMPUTER-NETWORKS-LAB-main/UDPClient.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+class UDPClient{
+    static DatagramSocket ds;
+    static DatagramPacket dp;
+    static BufferedReader br;
+    static InetAddress ia;
+    static byte[] b = new byte[1024];
+    static int cport = 6363, sport = 6464;
+
+    public static void main(String... args) throws Exception{
+        ds = new DatagramSocket(cport);
+        dp = new DatagramPacket(b,b.length);
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ia = InetAddress.getLocalHost();
+        System.out.println("Client is Running ... Type'STOP' to Quit");
+        while(true){
+            String str = new String(br.readLine());
+            b = str.getBytes();
+            if( str.equals("STOP"))
+            {
+                System.out.println("Terminated...");
+                ds.send(new DatagramPacket(b,b.length,ia,sport));
+                break;
+            }
+            ds.send(new DatagramPacket(b, b.length,ia,sport));
+            ds.receive(dp);
+            str = new String(dp.getData());
+            System.out.println("Server: "+str);
+        }
+    }
+
+}
---
+++ COMPUTER-NETWORKS-LAB-main/UDPServer.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+class UDPServer{
+    static DatagramSocket ds;
+    static DatagramPacket dp;
+    static BufferedReader br;
+    static InetAddress ia;
+    static byte[] b = new byte[1024];
+    static int cport = 6363;
+    static int sport = 6464;
+    
+    public static void main(String[] args) throws Exception{
+        ds = new DatagramSocket(sport);
+        dp = new DatagramPacket(b,b.length);
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ia = InetAddress.getLocalHost();
+        System.out.println("Server is Running .. Type STOP to Quit");
+        while(true){
+            String str = new String(br.readLine());
+            b = str.getBytes();
+            if( str.equals("STOP")){
+                System.out.println("Terminated..");
+                ds.send(new DatagramPacket(b,b.length,ia,cport));
+                break;
+            }
+
+            ds.send(new DatagramPacket(b,b.length,ia,cport));
+            ds.receive(dp);
+            str = new String(dp.getData());
+            System.out.println("Client: "+str);
+        }
+    }
+}
